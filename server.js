a567e03389c212d98d04c1a2df4dc91d64cf55df
charles-ramos <charles.ramos@fatec.sp.gov.br>
2021-11-04 11:19:38 -0300

    atualizacao de porta

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const serveStatic = require("serve-static")
 const path = require("path")
 const app = express()
 app.use(serveStatic(path.join(__dirname, "dist")))
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3001, function() {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
 })
