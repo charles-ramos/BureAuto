9c34d4214a74805fbbef06e423cbda61174f7330
charles-ramos <charles.ramos@fatec.sp.gov.br>
2021-11-04 11:09:08 -0300

    remocao do nodemon

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("./services/IoServices")
 
 const {httpServer} = require("./app")
-const port = process.env.PORT || 3001
 
 httpServer.listen(port, function() {
     console.log("Running on port", port)
