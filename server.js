8f51f2b327e3e6b5bf3e937b200dc7a0437d2956
charles-ramos <charles.ramos@fatec.sp.gov.br>
2021-11-04 09:26:37 -0300

    alteracao diretorio do server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const serveStatic = require("serve-static")
 const path = require("path")
-const app = express()
+app = express()
 app.use(serveStatic(path.join(__dirname, "dist")))
-app.listen(3000)
+const port = process.env.PORT || 3000
+app.listen(port)
+
